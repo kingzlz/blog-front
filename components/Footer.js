ae393f10d49a23059269c6eb3de2e43d88811948
zhanglizhu <kingzlz@163.com>
2019-12-17 15:35:33 +0800

    feat: init

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import '../public/style/components/footer.css';
+const Footer = () => (
+  <div className="footer-div">
+    <div>系统由 React+Node+Ant Desgin驱动 </div>
+    <div>这是网址地址.com</div>
+  </div>
+);
+
+export default Footer;
